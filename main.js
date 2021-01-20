b3b013444dcdf616ae44c4c2c5c731e62e5efa3c
praveen <55490713+praveensrautela@users.noreply.github.com>
2021-01-20 12:12:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// select element function
+const selectElement = function (element) {
+
+	return document.querySelector(element);
+
+};
+
+let menuToggler= selectElement('.menu_toggle');
+let body= selectElement('body');
+
+menuToggler.addEventListener('click', function(){
+		body.classList.toggle('open');
+		
+});
+
+$(document).on('click', 'ul li', function(){
+		$(this).addClass('active').siblings().removeClass('active')
+});
